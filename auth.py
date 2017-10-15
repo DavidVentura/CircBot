b6245624ac434f1788e820a1feb392dfb3461cc4
David <davidventura27@gmail.com>
2017-10-15 10:40:00 -0300

    refactor

--- auth.py
+++ auth.py
@@ -1,27 +1,30 @@
-#This script is licensed under the Apace 2.0 License (http://www.apache.org/licenses/LICENSE-2.0)
-#This script is a derivative work of the script at https://developers.google.com/api-client-library/python/auth/installed-app
+# This script is licensed under the Apace 2.0 License
+# This script is a derivative work of the script at
+# https://developers.google.com/api-client-library/python/auth/installed-app
 
-import json
-import webbrowser
-import configparser
 import httplib2
 from oauth2client import client
 
-flow = client.flow_from_clientsecrets(
-	'client_secrets.json',
-	scope='https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl',
-	redirect_uri='urn:ietf:wg:oauth:2.0:oob')
 
-auth_uri = flow.step1_get_authorize_url()
-print(auth_uri)
-webbrowser.open(auth_uri)
+def main():
+    flow = client.flow_from_clientsecrets(
+        'client_secrets.json',
+        scope='https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl',
+        redirect_uri='urn:ietf:wg:oauth:2.0:oob')
 
-print("Opening web browser to request auth code")
-auth_code = input('Enter the auth code: ')
+    auth_uri = flow.step1_get_authorize_url()
+    print(auth_uri)
 
-credentials = flow.step2_exchange(auth_code)
-http_auth = credentials.authorize(httplib2.Http())
+    print("Open the shown link")
+    auth_code = input('Enter the auth code: ')
 
-outFile = open("OAuthCredentials.json","w")
-outFile.write(str(credentials.to_json()))
-outFile.close()
+    credentials = flow.step2_exchange(auth_code)
+    credentials.authorize(httplib2.Http())
+
+    outFile = open("OAuthCredentials.json", "w")
+    outFile.write(str(credentials.to_json()))
+    outFile.close()
+
+
+if __name__ == '__main__':
+    main()
